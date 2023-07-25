7e2f89e196f742d7f0c2acf194cc12067569d326
oleksandrhfnr <argamakov2213@gmail.com>
2023-07-25 23:22:17 +0300

    formatting files

--- homework3/app.js
+++ homework3/app.js
@@ -7,7 +7,6 @@
 //         console.log(arr[i]);
 //     }
 //     return newArr;
-    
 
 // }
 
@@ -15,59 +14,55 @@
 
 // task 2
 
-function createArray(a,b) {
-    let arr = [];
-    if (a > b) {
-        console.log("Wrong numbers");
-    } else {
-        let num = a;
-        
-        while (num <= b) {
-            arr.push(num);
-            num++;
-        }
+function createArray(a, b) {
+  let arr = [];
+  if (a > b) {
+    console.log("Wrong numbers");
+  } else {
+    let num = a;
+
+    while (num <= b) {
+      arr.push(num);
+      num++;
     }
-    return arr;
+  }
+  return arr;
 }
 
-console.log("Task 2: ", createArray(2,9));
+console.log("Task 2: ", createArray(2, 9));
 
 // task 3
 
-function createLongArray(a,b) {
-    let arr = [];
-    if (a > b) {
-        console.log("Wrong numbers");
-    } else {
-        let num = a;
-        while (num <= b) {
-            for (let i = 0; i<num; i++) {
-                arr.push(num);
-            }
-            num++;
-        }
+function createLongArray(a, b) {
+  let arr = [];
+  if (a > b) {
+    console.log("Wrong numbers");
+  } else {
+    let num = a;
+    while (num <= b) {
+      for (let i = 0; i < num; i++) {
+        arr.push(num);
+      }
+      num++;
     }
-    return arr;
+  }
+  return arr;
 }
 
-console.log("Task 3: ", createLongArray(2,9));
+console.log("Task 3: ", createLongArray(2, 9));
 
 // task 4
 
 function Random(k) {
-    const min = 1;
-    const max = 500;
-    let randomArr = [];
-    for (let i=0; i <k;i++) {
-        randomArr.push(Math.floor(Math.random()*(max-min)+min));
-    }
-    return randomArr;
+  const min = 1;
+  const max = 500;
+  let randomArr = [];
+  for (let i = 0; i < k; i++) {
+    randomArr.push(Math.floor(Math.random() * (max - min) + min));
+  }
+  return randomArr;
 }
 
 console.log("Task 4:", Random(6));
 
 // Task 5
-
-
-
-
