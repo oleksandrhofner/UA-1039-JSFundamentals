485277de25ce0f9160ba1ad98ef767a88489e0ef
oleksandrhfnr <argamakov2213@gmail.com>
2023-08-02 11:10:03 +0300

    task 2

--- homework4/app.js
+++ homework4/app.js
@@ -1,26 +1,56 @@
 // Task 1
 
-const arr = [2, 5, 10, 3, 4, 3, 7, 2];
-function sumSliceArray(arr, first, second) {
-  try {
-    const firstNum = first;
-    const secondNum = second;
-    if (typeof firstNum && typeof secondNum !== "number"){
-        throw new SyntaxError("first number and second number can`t include string");
-    }
-    if (arr[firstNum] > arr.length || arr[secondNum] > arr.length) {
-        throw new SyntaxError("number is too long");
-    }
+// const arr = [2, 5, 10, 3, 4, 3, 7, 2];
+// function sumSliceArray(arr, first, second) {
+//   try {
+//     const firstNum = first;
+//     const secondNum = second;
+//     if (typeof firstNum && typeof secondNum !== "number"){
+//         throw new SyntaxError("first number and second number can`t include string");
+//     }
+//     if (arr[firstNum] > arr.length || arr[secondNum] > arr.length) {
+//         throw new SyntaxError("number is too long");
+//     }
     
-    return Math.floor(Number(arr[firstNum]) + Number(arr[secondNum]));
+//     return Math.floor(Number(arr[firstNum]) + Number(arr[secondNum]));
 
-  } catch (error) {
-    console.log("Hmm, we have error... : ", error);
-    return 0;
-  }
-}
+//   } catch (error) {
+//     console.log("Hmm, we have error... : ", error);
+//     return 0;
+//   }
+// }
 
-console.log("sumSlice:", sumSliceArray(arr, 3, 7));
+// console.log("sumSlice:", sumSliceArray(arr, 3, 7));
 
 // Task 2
+
+//   const userName = prompt("What`s your name?");
+//   const userAge = prompt("How old are you?");
+//   const userStatus = prompt("Your status?");
+// function checkAge(userName,userAge,userStatus) {
+//   try {
+//     if (userName === "" || userAge === "" || userStatus === "") {
+//       throw new Error("The field is empty! Please enter your age!");
+//     }
+//     if (userAge < 18 || userAge > 70) {
+//       throw new RangeError("Age must be correct");
+//     }
+//     if (userStatus === "Admin" || userStatus === "Moderator" || userStatus === "User") {
+//       console.log("You can watch films");
+//     } else {
+//     throw new EvalError("Must be: Admin, Moderator or User");
+
+//     }
+
+//       return console.log(`Welcome, ${userName}, you are ${userAge} old, your status is ${userStatus}!`);
+    
+//   } catch (error) {
+//     console.log("Error: ", error);
+//   }
+// }
+
+// checkAge(userName,userAge,userStatus);
+
+
+
 
