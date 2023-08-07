9d49ea7789d088afcd656adca9860ce35e8cfdb5
oleksandrhfnr <argamakov2213@gmail.com>
2023-08-07 23:01:11 +0300

    task 2

--- homework6/app.js
+++ homework6/app.js
@@ -1,22 +1,41 @@
 // Task 1
 
-const list = document.querySelector("#list");
+// const list = document.querySelector("#list");
 
-const firstElement = list.firstElementChild;
-console.log("first element:", firstElement.textContent);
+// const firstElement = list.firstElementChild;
+// console.log("first element:", firstElement.textContent);
 
-const lastElement = list.lastElementChild;
-console.log("last element:", lastElement.textContent);
+// const lastElement = list.lastElementChild;
+// console.log("last element:", lastElement.textContent);
 
-const secondElement = firstElement.nextElementSibling;
-console.log("second element:", secondElement.textContent);
+// const secondElement = firstElement.nextElementSibling;
+// console.log("second element:", secondElement.textContent);
 
-const fourthElement = lastElement.previousElementSibling;
-console.log("fourth element:", fourthElement.textContent);
+// const fourthElement = lastElement.previousElementSibling;
+// console.log("fourth element:", fourthElement.textContent);
 
-const thirdthElement = list.childNodes[5];
-console.log("third element:", thirdthElement.textContent);
+// const thirdthElement = list.childNodes[5];
+// console.log("third element:", thirdthElement.textContent);
 
-console.log(
-  `All elements:  ${firstElement.textContent},${lastElement.textContent},${secondElement.textContent},${fourthElement.textContent},${thirdthElement.textContent}`
-);
+// console.log(
+//   `All elements:  ${firstElement.textContent},${lastElement.textContent},${secondElement.textContent},${fourthElement.textContent},${thirdthElement.textContent}`
+// );
+
+// Task 2
+
+// const header = document.querySelector('h1');
+// header.style.backgroundColor = 'green';
+// const div = document.querySelector('#myDiv');
+// const firstParagraph = document.querySelector('p');
+// firstParagraph.style.fontWeight = 'bold';
+// const secondParagraph = firstParagraph.nextElementSibling;
+// secondParagraph.style.color = 'red';
+// const thirdParagraph = secondParagraph.nextElementSibling;
+// thirdParagraph.style.textDecoration = 'underline';
+// const fourthParagraph = thirdParagraph.nextElementSibling;
+// fourthParagraph.style.fontStyle = 'italic';
+// const unorderedList = document.querySelector('#myList');
+// unorderedList.style.listStyle = 'none';
+// unorderedList.style.display = 'flex';
+// const span = document.querySelector('span');
+// span.style.visibility = 'hidden';
