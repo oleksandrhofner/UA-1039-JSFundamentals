62f7396e3467dfd41f33f9b6c07c999194dd8c5b
oleksandrhfnr <argamakov2213@gmail.com>
2023-08-10 20:58:42 +0300

    task 1

--- homework7/app.js
+++ homework7/app.js
@@ -1,1 +1,23 @@
 
+// const w3schools = window.open("https://www.w3schools.com","_blank","width=200,height=200");
+// Task 1.1
+
+// const alert1 = alert(w3schools);
+
+// Task 1.2
+
+// const alert2 = setTimeout(function(){
+//     alert(w3schools.resizeTo(500,500));
+// },2000);
+
+// Task 1.3
+
+// const alert3 = setTimeout(function(){
+//     w3schools.moveTo(200,2000);
+// },2000);
+
+// Task 1.4
+
+// const alert4 = setTimeout(function(){
+//     w3schools.close();
+// },2000);
