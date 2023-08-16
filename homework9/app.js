aa240bc8c651ec45480827edd967185fce23c88b
oleksandrhfnr <argamakov2213@gmail.com>
2023-08-16 17:35:45 +0300

    task 2

--- homework9/app.js
+++ homework9/app.js
@@ -1,7 +1,17 @@
 
 // Task 1
 
-$(function(){
-    $("h2.head").css("backgroundColor", "green");
-    $("span.inner").css("fontSize", "35px");
-})
+// $(function(){
+//     $("h2.head").css("backgroundColor", "green");
+//     $("span.inner").css("fontSize", "35px");
+// })
+
+// Task 2
+
+// $(function(){
+//     $("a").on("click",function(){
+//         if ($("a").prop("href") == 'https://www.google.com/') {
+//             $("a").attr("target", "_blank");
+//         }
+//     })
+// })
