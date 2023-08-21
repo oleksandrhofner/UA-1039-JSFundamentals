4e20940792444fadf24dd3a9591be02adcf50a82
oleksandrhofner <argamakov2213@gmail.com>
2023-08-21 23:04:24 +0300

    create server

--- homework12/app.js
+++ homework12/app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const http = require("http");
+
+http.createServer(function(request,response) {
+    response.setHeader("Content-Type", "text/html");
+    response.write("<p>Hello world</p>");
+    response.end();
+}).listen(5000);
