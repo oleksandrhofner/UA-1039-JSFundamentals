3b7acd40d6ab38dd455fcfe850dd27253fb865fc
oleksandrhofner <argamakov2213@gmail.com>
2023-08-22 09:48:47 +0300

    add task1

--- homework12/app.js
+++ homework12/app.js
@@ -1,7 +1,16 @@
-const http = require("http");
+// Task 1
 
-http.createServer(function(request,response) {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<p>Hello world</p>");
-    response.end();
-}).listen(5000);
+// const http = require("http");
+// const os = require("os");
+// http.createServer(function(request,response) {
+//     response.setHeader("Content-Type", "text/html");
+//     response.write(`<p>User name: ${os.userInfo().username}</p>`);
+//     response.write(`<p>OS type: ${os.platform}</p>`);
+//     response.write(`<p>System work time: ${os.uptime}</p>`);
+//     response.write(`<p>Current work directory: ${__dirname}</p>`);
+//     response.end();
+// }).listen(5000);
+
+// Task 2
+
+
