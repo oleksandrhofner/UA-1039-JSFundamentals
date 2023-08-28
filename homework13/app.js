cec14dc0e9d1943a89e6d8e673eed58c6656e8e3
oleksandrhofner <argamakov2213@gmail.com>
2023-08-28 22:48:20 +0300

    add task 3

--- homework13/app.js
+++ homework13/app.js
@@ -35,3 +35,43 @@
 //   }
 // }
 // downloadBook();
+
+// Task 3
+
+// async function randomUser() {
+//   const url = "https://randomuser.me/api/";
+//   const response = await fetch(url);
+
+//   if (response.ok) {
+//     const json = await response.json();
+//     const btn = document.querySelector(".btn");
+//     const unorderedList = document.createElement("ul");
+//     let user = json.results[0];
+//     btn.addEventListener("click", function() {
+        
+//       let nameItem = document.createElement("li");
+//       nameItem.textContent = `Name: ${user.name.first} ${user.name.last}`;
+
+//       let genderItem = document.createElement("li");
+//       genderItem.textContent = `Gender: ${user.gender}`;
+
+//       let locationItem = document.createElement("li");
+//       locationItem.textContent = `Location: ${user.location.city}, ${user.location.country}`;
+
+//       let loginItem = document.createElement("li");
+//       loginItem.textContent = `Login: ${user.login.username}`;
+
+//       let emailItem = document.createElement("li");
+//       emailItem.textContent = `Email: ${user.email}`;
+
+//       unorderedList.appendChild(nameItem);
+//       unorderedList.appendChild(genderItem);
+//       unorderedList.appendChild(locationItem);
+//       unorderedList.appendChild(loginItem);
+//       unorderedList.appendChild(emailItem);
+//       document.body.appendChild(unorderedList);
+//     });
+//   }
+// }
+
+// randomUser();
